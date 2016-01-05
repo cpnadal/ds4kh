d79f6f8fd5f090a3f6d9f05458e8978b62271115
PIERRE-NADAL Cyrille <cyrille@pierre-nadal.com>
2016-01-05 11:02:42 +0100

    allègement du client

--- js/script_afficheur.php
+++ js/script_afficheur.php
@@ -1,11 +1,10 @@
 <script>
 var XML_filename = "data.xml";
 function serverAutoRun() {
-	setInterval(function(){showDateTime()},100); //run showDateTime function every second
-	document.bgColor="black"; //set background to black
+	document.bgColor="black";
 	Current_fileTime= getFiletime();
-	setInterval(function(){fileIsModified()},100); //run fileIsModified function every 5 seconds
-	loadSettings();	//load moving message & currencies
+	setInterval(function(){fileIsModified()},5000);
+	loadSettings();
 }
 
 function fileIsModified() {
@@ -39,23 +38,10 @@ function loadSettings() {
 		variable1 = x[0].childNodes[0].nodeValue;
 		document.getElementById("text").innerHTML = '<img src="'+variable1+'"  />';
 	}
-	if(variable0=="video") {
-		x=xmlDoc.getElementsByTagName("VIDEO");
-		variable1 = x[0].childNodes[0].nodeValue;
-		document.getElementById("text").innerHTML = '<video width="100%" height="100%" autoplay ><source src="'+variable1+'" type="video/mp4"></video>';
-	}
 	if(variable0!="image" && variable0!="video") {
 		document.getElementById("text").innerHTML = variable0;
 	}
-	x=xmlDoc.getElementsByTagName("DATETIME");
-	show_datetime = x[0].childNodes[0].nodeValue;
-	if(show_datetime==1) {
-		document.getElementById("datetime").style.display = 'block';
-		showDateTime();
-	} else {
-		document.getElementById("datetime").style.display = 'none';
-	}
-		$(document).ready(function () {
+	$(document).ready(function () {
 		$(".imgLiquidFill").imgLiquid({fill:true});
 	});
 
@@ -69,36 +55,5 @@ function create_XMLHttpRequest() {
 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	return xmlhttp;
-}
-
-function showDateTime() {
-	dateTimeNow = new Date(); 
-
-	weekday = new Array(7);
-	weekday[0]=  "Dimanche";
-	weekday[1] = "Lundi";
-	weekday[2] = "Mardi";
-	weekday[3] = "Mercredi";
-	weekday[4] = "Jeudi";
-	weekday[5] = "Vendredi";
-	weekday[6] = "Samedi";
-	weekdayNow = weekday[dateTimeNow.getDay()];
-	
-	dd = dateTimeNow.getDate(); 
-	mm = dateTimeNow.getMonth()+1;//January is 0! 
-	yyyy = dateTimeNow.getFullYear(); 
-	if(dd<10){dd='0'+dd};
-	if(mm<10){mm='0'+mm}; 
-	dateNow= weekdayNow + " " + dd + "/" + mm + "/" + yyyy;
-	document.getElementById("dateNow").innerHTML = dateNow; 
-	
-	hh = dateTimeNow.getHours();
-	nn = dateTimeNow.getMinutes();
-	ss = dateTimeNow.getSeconds();
-	if(hh<10){hh='0'+hh};
-	if(nn<10){nn='0'+nn}; 	
-	if(ss<10){ss='0'+ss}; 	
-	timeNow = hh + ":" + nn + ":" + ss;
-	document.getElementById("timeNow").innerHTML = timeNow; 
 }
 </script>
--- server.php
+++ server.php
@@ -1,28 +1,7 @@
-<?php include('inc/fonctions.php'); ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
-<html>
-<head>
-<title>Digital Signage for Kingdom Hall</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script src="js/jquery-2.1.4.js"></script>
-<script src="js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
-<script src="js/tinymce_4.3.2/tinymce.min.js"></script>
-<link rel="stylesheet" href="js/jquery-ui-1.11.4/jquery-ui.theme.css">
-  <link rel="stylesheet" href="js/bootstrap-3.3.6-dist/css/bootstrap.css">
-<script src="js/bootstrap-3.3.6-dist/js/bootstrap.js"></script>
-<script src="js/bootstrap.file-input.js"></script>
-<link rel="stylesheet" href="css/ds4kh.css">
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/imgLiquid.js"></script>
 <?php include('js/script_afficheur.php'); ?>
-</head>
 <body onLoad="serverAutoRun()">
-		<div id="text" class="text imgLiquidFill imgLiquid full" align="center" >
-		</div>
-	<div id="datetime" class="footer" style="color:#FFFFFF;font-size:24px;">
-	<span>Nous somme le </span>
-	<span id="dateNow"></span>
-	<span>. Il est </span>
-	<span id="timeNow"></span>
-</div>
+	<div id="text" style="width:100%;height:100%;color:#FFF" class="imgLiquidFill" align="center" ></div>
 </body>
-</html>
 
